f2a561257e481f13672b9e4070fa48a3d94017f9
mrsterner <simon@sterner.dev>
2022-03-20 14:17:51 +0100

    Shuffedness is back :tm:

--- src/main/java/net/arathain/tot/client/entity/renderer/layer/DriderChestplateLayer.java
+++ src/main/java/net/arathain/tot/client/entity/renderer/layer/DriderChestplateLayer.java
@@ -9,6 +9,7 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.DyeableArmorItem;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.geo.render.built.GeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
 
@@ -22,7 +23,10 @@ public class DriderChestplateLayer extends GeoLayerRenderer<DriderEntity> {
     public void render(MatrixStack matrixStackIn, VertexConsumerProvider bufferIn, int packedLightIn, DriderEntity entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         if(!entitylivingbaseIn.getEquippedStack(EquipmentSlot.CHEST).isEmpty()) {
             Identifier location = new Identifier(TomeOfTiamatha.MODID, "textures/entity/drider/armor/" + entitylivingbaseIn.getEquippedStack(EquipmentSlot.CHEST).getItem().toString() + ".png");
-            RenderLayer armor = RenderLayer.getEntityTranslucent(location, true);
+            RenderLayer armor = RenderLayer.getArmorCutoutNoCull(location);
+            var model = this.getEntityModel().getModel(this.getEntityModel().getModelLocation(entitylivingbaseIn));
+            model.getBone("spider").get().setHidden(true);
+            model.getBone("leftarm").get().setHidden(true);
             if (entitylivingbaseIn.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof DyeableArmorItem) {
                 int i = ((DyeableArmorItem)entitylivingbaseIn.getEquippedStack(EquipmentSlot.CHEST).getItem()).getColor((entitylivingbaseIn.getEquippedStack(EquipmentSlot.CHEST)));
                 float f = (float)(i >> 16 & 0xFF) / 255.0F;
@@ -33,6 +37,8 @@ public class DriderChestplateLayer extends GeoLayerRenderer<DriderEntity> {
             else {
                 this.getRenderer().render(this.getEntityModel().getModel(this.getEntityModel().getModelLocation(entitylivingbaseIn)), entitylivingbaseIn, partialTicks, armor, matrixStackIn, bufferIn, bufferIn.getBuffer(armor), packedLightIn, OverlayTexture.DEFAULT_UV, 1.0f, 1.0f, 1.0f, 1.0f);
             }
+            model.getBone("spider").get().setHidden(false);
+            model.getBone("leftarm").get().setHidden(false);
         }
     }
 
