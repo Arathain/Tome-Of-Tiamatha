b27f3427790e41028565a3096c3cf88aca43f6a6
Arathain <arathainfarqoe@gmail.com>
2022-05-09 09:08:02 +0200

    ice cream machine broke

---
+++ src/main/java/net/arathain/tot/client/entity/renderer/raven/NevermoreEntityRenderer.java
@@ -0,0 +1,13 @@
+package net.arathain.tot.client.entity.renderer.raven;
+
+import net.arathain.tot.client.entity.model.raven.NevermoreEntityModel;
+import net.arathain.tot.common.entity.living.raven.NevermoreEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class NevermoreEntityRenderer extends GeoEntityRenderer<NevermoreEntity> {
+    public NevermoreEntityRenderer(EntityRendererFactory.Context ctx) {
+        super(ctx, new NevermoreEntityModel());
+    }
+}
